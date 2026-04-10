a6c3df7a6c1891c86bb2ebf38dfcc179fedb643e
dvorst <87502756+dvorst@users.noreply.github.com>
2026-04-10 11:53:32 +0000

    test: fix blesh test, busybox grep only supports shorthand flags

--- test/blesh/test.sh
+++ test/blesh/test.sh
@@ -12,7 +12,12 @@ bashrc_line='[[ $- == *i* ]] && source '"$INSTALL_DIR"'/blesh/ble.sh'
 check "ble.sh is installed" test -f "${INSTALL_DIR}/blesh/ble.sh"
 check "ble.sh is readable" test -r "${INSTALL_DIR}/blesh/ble.sh"
 check "bash.bashrc exists" test -f "${BASHRC}"
-check "bash.bashrc contains integration line" grep --fixed-strings --quiet "${bashrc_line}" "${BASHRC}"
+
+# busybox grep only supports shorthand flags
+#	-F: literal strings, not regex
+#	-q: quiet, returns 0 if found, 1 otherwise.
+check "bash.bashrc contains integration line" grep -Fq "${bashrc_line}" "${BASHRC}"
+
 # --lib will 'Only load ble.sh and do nothing else', so it does not attach to the terminal session, allowing it to be run in a non-interactive/non-TTY environment
 check "ble.sh can be loaded by bash" bash -c "source ${INSTALL_DIR}/blesh/ble.sh --lib"
 
