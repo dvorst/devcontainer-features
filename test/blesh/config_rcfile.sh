52db5ead9d07d9835cc7687645fce3a1477fd858
dvorst <87502756+dvorst@users.noreply.github.com>
2026-04-24 08:23:44 +0000

    test: bleopt config_rcfile, wip

--- test/blesh/config_rcfile.sh
+++ test/blesh/config_rcfile.sh
@@ -8,21 +8,22 @@ RCFILE="/workspaces/some-project/.blerc"  # see scenarios.json
 
 
 get-bleopt-rcfile() {
-	# Load bashrc and get rcfile
-	# cannot use `bash -i -c "source $BASHRC && bleopt rcfile"` because:
+	# Load bashrc and get blesh rcfile
+	# cannot use `bash -i -c "bleopt rcfile"` because:
 	# 	ble.sh bails out when BASH_EXECUTION_STRING is set (bash -c), so the command must be run
 	#	via a script file.
 	# cannot use `bash -i "$_tmp_script_file` because:
 	#	ble.sh bails out when the process is not connected to a TTY, so `script -qec` is used
-	#	which does have TTY attached. `bash -i` is still needed to 
+	#	which does have TTY attached. `bash -i` is still needed too
 	# script
 	#	-q: suppres script started/done header/footer that script normally prints
 	#	-e: exit code passthrough
 	#	-c: take argument to run as command instead of spawning an interactive shell
+	# tail -n 1: return only the rcfile, no messages from sourcing ble.sh
+	# tr -d '\r' removes the carriage return
 	_tmp_script_file=$(mktemp)
 	printf 'bleopt rcfile' > "${_tmp_script_file}"
-	script -qec "bash -i $_tmp_script_file" /dev/null
-	echo "$rcfile"
+	script -qec "bash -i $_tmp_script_file" /dev/null | tail -n 1 | tr -d '\r'
 }
 
 get-bleopt-rcfile
